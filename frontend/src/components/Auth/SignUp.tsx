cebee4649b06f9bfb92074b663b802b613d1c3d8
JAYLAM <>
2023-10-24 16:37:54 -0400

    Updated SignUp.

--- frontend/src/components/Auth/SignUp.tsx
+++ frontend/src/components/Auth/SignUp.tsx
@@ -1,4 +1,7 @@
-import { useState, Fragment } from "react";
+import { useState, useEffect, useRef, Fragment } from "react";
+import { registerUser, RegistrationData } from "../../feature/auth/authActions";
+import { useAppDispatch, useAppSelector } from "../../app/hook";
+
 import {
   Button,
   Dialog,
@@ -11,19 +14,74 @@ import {
 } from "@mui/material";
 
 const SignUp = () => {
+  const { loading, success } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
+  const previousLoading = useRef(loading);
+
   const [open, setOpen] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
+  const initialRegisterData: RegistrationData = {
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const [registerData, setRegisterData] = useState(initialRegisterData);
+
+  useEffect(() => {
+    console.log("SignUp - useEffect - success", success);
+    console.log("SignUp - useEffect: loading", loading);
+    if (loading !== previousLoading.current && !loading) {
+      if (success) {
+        console.log("SignUp - success");
+        handleClose();
+      } else {
+        console.log("SignUp - fail");
+        setErrorMsg("Sign up fail. Please try again.");
+        setOpen(true);
+      }
+    }
+  }, [loading]);
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-  const handleClickSignUp = () => {
+  const handleClose = () => {
+    setErrorMsg("");
+    setRegisterData(initialRegisterData);
     setOpen(false);
   };
 
-  const handleClickCancel = () => {
-    setOpen(false);
+  const handleClickSignUp = () => {
+    setErrorMsg("");
+    if (!isAllFillin()) {
+      setErrorMsg("Username, email or password is missing.");
+    } else if (!isEmailValid()) {
+      setErrorMsg("Please input valid email.");
+    } else if (!arePasswordsMatching()) {
+      setErrorMsg("Password not matching.");
+    } else {
+      console.log("SignUp - registerUser", registerData);
+      dispatch(registerUser(registerData));
+    }
+  };
+
+  const isAllFillin = () => {
+    return Object.values(registerData).every((value) => !!value);
+  };
+
+  const isEmailValid = () => {
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    return emailRegex.test(registerData.email);
+  };
+
+  const arePasswordsMatching = () => {
+    return registerData.password === registerData.confirmPassword;
+  };
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterData({ ...registerData, [event.target.id]: event.target.value });
   };
 
   return (
@@ -42,6 +100,8 @@ const SignUp = () => {
             type="username"
             fullWidth
             variant="standard"
+            value={registerData.username}
+            onChange={handleInput}
           />
           <TextField
             autoFocus
@@ -51,6 +111,8 @@ const SignUp = () => {
             type="email"
             fullWidth
             variant="standard"
+            value={registerData.email}
+            onChange={handleInput}
           />
           <TextField
             autoFocus
@@ -60,15 +122,19 @@ const SignUp = () => {
             type="password"
             fullWidth
             variant="standard"
+            value={registerData.password}
+            onChange={handleInput}
           />
           <TextField
             autoFocus
             margin="dense"
-            id="confirm-password"
+            id="confirmPassword"
             label="Confirm Password"
             type="password"
             fullWidth
             variant="standard"
+            value={registerData.confirmPassword}
+            onChange={handleInput}
           />
           {errorMsg && (
             <DialogContentText
@@ -89,6 +155,7 @@ const SignUp = () => {
                 variant="contained"
                 fullWidth={true}
                 onClick={handleClickSignUp}
+                disabled={loading}
               >
                 Sign Up
               </Button>
@@ -98,7 +165,8 @@ const SignUp = () => {
                 variant="contained"
                 fullWidth={true}
                 color="error"
-                onClick={handleClickCancel}
+                onClick={handleClose}
+                disabled={loading}
               >
                 Cancel
               </Button>
--- frontend/src/feature/auth/authActions.ts
+++ frontend/src/feature/auth/authActions.ts
@@ -7,6 +7,7 @@ export interface RegistrationData {
   username: string;
   email: string;
   password: string;
+  confirmPassword: string;
 }
 
 export interface LoginData {
