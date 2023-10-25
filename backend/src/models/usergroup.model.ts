5bd59f2475522839fff6ed97dd1fe0bfaa95da1c
Timothy Lam <timothylam1228@gmail.com>
2023-10-25 16:08:15 -0400

    update group schema

--- backend/src/models/usergroup.model.ts
+++ backend/src/models/usergroup.model.ts
@@ -3,12 +3,8 @@ import bcrypt from 'bcrypt';
 
 // Define the user schema
 
-const userSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the user
-});
-
 const userGroupSchema = new Schema({
-  users: [userSchema],
+  users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   creator: { type: Schema.Types.ObjectId, ref: 'User' }, // Reference to the user who created the event
   code: { type: String, required: true, unique: true },
   // Add more fields as needed
