6f180a127cbb74af494a25b0f8f656adc3fecafb
JAYLAM <>
2023-10-26 22:28:48 -0400

    Updated NewExpense.

--- frontend/src/components/SplitExpense/NewExpense.tsx
+++ frontend/src/components/SplitExpense/NewExpense.tsx
@@ -1,40 +1,121 @@
-import { Button, Grid, TextField } from "@mui/material";
-import { useAppDispatch } from "../../app/hook";
+import { useState, Fragment } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  TextField,
+} from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../app/hook";
 import {
   createExpenseGroup,
   CreateExpenseType,
   ExpenseType,
+  listExpense,
 } from "../../feature/expense/expenseActions";
-import { useCallback, useState } from "react";
+
 const NewExpense = () => {
   const dispatch = useAppDispatch();
+  const { loading, success, error } = useAppSelector((state) => state.event);
 
+  const [open, setOpen] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
   const [expenseData, setExpenseData] = useState<CreateExpenseType>({
     title: "",
     type: ExpenseType.GROUP,
   });
+
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setExpenseData({ ...expenseData, [event.target.id]: event.target.value });
   };
-  const handleClickCreate = useCallback(() => {
-    dispatch(createExpenseGroup(expenseData));
-  }, []);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setErrorMsg("");
+    expenseData.title = "";
+  };
+
+  const handleClickCreate = () => {
+    if (!expenseData.title) {
+      setErrorMsg("Title is missing.");
+      return;
+    }
+
+    dispatch(createExpenseGroup(expenseData)).then(() => {
+      dispatch(listExpense());
+      if (success) {
+        handleClose();
+      } else {
+        setErrorMsg(error ?? "Create Failed. Please try again.");
+      }
+    });
+  };
+
   return (
-    <Grid>
-      <Grid>Expense Name</Grid>
-      <TextField
-        autoFocus
-        margin="dense"
-        id="title"
-        label="Title"
-        type="title"
-        fullWidth
-        variant="standard"
-        value={expenseData.title}
-        onChange={handleInput}
-      />
-      <Button onClick={handleClickCreate}>Create</Button>
-    </Grid>
+    <Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        New Expense
+      </Button>
+      <Dialog fullWidth={true} maxWidth={"lg"} open={open}>
+        <DialogTitle>Create New Event</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="title"
+            label="Title"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={expenseData.title}
+            onChange={handleInput}
+          />
+          {errorMsg && (
+            <DialogContentText
+              sx={{
+                color: "#f14444",
+                width: "100%",
+                mt: "10px",
+              }}
+            >
+              {errorMsg}
+            </DialogContentText>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Grid container px={4} pb={3} spacing={2}>
+            <Grid item xs={6}>
+              <Button
+                variant="contained"
+                fullWidth={true}
+                color="error"
+                onClick={handleClose}
+              >
+                Cancel
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button
+                variant="contained"
+                fullWidth={true}
+                onClick={handleClickCreate}
+                disabled={loading}
+              >
+                Create
+              </Button>
+            </Grid>
+          </Grid>
+        </DialogActions>
+      </Dialog>
+    </Fragment>
   );
 };
 
